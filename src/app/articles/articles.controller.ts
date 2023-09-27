7d1f6dcf75681f2495854af07136cfd8c8002ee1
Yuval Levi <yuvall@moveohls.com>
2023-09-27 14:15:21 +0300

    top headlines worked

--- src/app/articles/articles.controller.ts
+++ src/app/articles/articles.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Get, Query, Res } from '@nestjs/common';
+
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  Query,
+  Res,
+} from '@nestjs/common';
 import { ArticlesService } from './articles.service';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Response } from 'express';
@@ -7,18 +14,43 @@ import { Response } from 'express';
 export class ArticlesController {
   constructor(private readonly articlesService: ArticlesService) {}
 
-  @Get()
-  @ApiResponse({
-    isArray: true,
-    status: 200,
-    description: 'Get list of articles',
-  })
-  async articles(
+  //   @Get()
+  //   @ApiResponse({
+  //     isArray: true,
+  //     status: 200,
+  //     description: 'Get list of articles',
+  //   })
+  //   async articles(
+  //     @Query('pageSize') pageSize: number,
+  //     @Query('page') page: number,
+  //     @Res() res: Response,
+  //   ) {
+  //     const result = await this.articlesService.findArticles(pageSize, page);
+  //     return res.status(200).send(result);
+  //   }
+
+  @Get('top-headlines')
+  async topHeadlines(
     @Query('pageSize') pageSize: number,
     @Query('page') page: number,
+    @Query('country') country: string,
+    @Query('source') source: string,
+    @Query('category') category: string,
     @Res() res: Response,
   ) {
-    const result = await this.articlesService.findArticles(pageSize, page);
+    if ((country || category) && source) {
+      throw new BadRequestException(
+        "You cannot use 'country' or 'category' and 'source' together.",
+      );
+    }
+    const result = await this.articlesService.findTopHeadlines(
+      pageSize,
+      page,
+      country,
+      source,
+      category,
+    );
     return res.status(200).send(result);
   }
 }
+
--- src/app/articles/articles.service.ts
+++ src/app/articles/articles.service.ts
@@ -1,3 +1,4 @@
+
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
@@ -7,25 +8,61 @@ import { ArticleFront, articleFrontProjection } from '../helpers';
 export class ArticlesService {
   constructor(@InjectModel('Article') private articleModel: Model<Article>) {}
 
-  async findArticles(
+  //   async findArticles(
+  //     pageSize: number,
+  //     page: number,
+  //     country,
+  //     source,
+  //     type: 'top-headlines' | 'everything',
+  //   ): Promise<{ articles: ArticleFront[]; totalResults: number }> {
+  //     // Convert to number and apply defaults if necessary
+  //     pageSize = Number(pageSize) || 10;
+  //     page = Number(page) || 1;
+  //     let query = {};
+  //     const skip = (page - 1) * pageSize;
+  //     const articlesRes = await this.articleModel
+  //       .find()
+  //       .select(articleFrontProjection)
+  //       .limit(pageSize)
+  //       .skip(skip)
+  //       .exec();
+  //     const totalResults = articlesRes.length;
+  //     return {
+  //       articles: articlesRes,
+  //       totalResults: totalResults,
+  //     };
+  //   }
+  // }
+  async findTopHeadlines(
     pageSize: number,
     page: number,
+    country: string,
+    source: string,
+    category: string,
   ): Promise<{ articles: ArticleFront[]; totalResults: number }> {
     // Convert to number and apply defaults if necessary
     pageSize = Number(pageSize) || 10;
     page = Number(page) || 1;
 
+    let query: any = { isTopheadlines: true };
+
+    if (country) query.country = country;
+    if (source) query.source = source;
+    if (category) query.category = category;
+    console.log(query);
     const skip = (page - 1) * pageSize;
     const articlesRes = await this.articleModel
-      .find()
+      .find(query)
       .select(articleFrontProjection)
       .limit(pageSize)
       .skip(skip)
       .exec();
     const totalResults = articlesRes.length;
+
     return {
       articles: articlesRes,
       totalResults: totalResults,
     };
   }
 }
+
